93653167efd6601db99cc21aaee059b6edfa3c34
Sakib Sami <s4kibs4mi@gmail.com>
2021-01-12 01:23:45 +0600

    - contract implemented (partially)

--- contracts/NewsChain.sol
+++ contracts/NewsChain.sol
@@ -2,4 +2,36 @@ pragma solidity ^0.5.16;
 
 contract NewsChain {
 
+    struct Writer {
+        string name;
+        string title;
+        string password;
+        int created_at;
+    }
+
+    struct Post {
+        string title;
+        string details;
+        address writer;
+        string[] tags;
+        int created_at;
+    }
+
+    address owner;
+
+    mapping(address => Writer) writers;
+    mapping(address => Post) posts;
+    mapping(string => bool) isWriterExists;
+
+    string portalName;
+
+    function createAuthor() public returns (bool){
+
+        return false;
+    }
+
+    function createPost() public returns (bool) {
+
+        return false;
+    }
 }
